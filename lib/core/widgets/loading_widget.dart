50bb04b35c1d263609b338aecec01e0fcbf3f4ee
Ahmed okal <159727160+don-okal@users.noreply.github.com>
2025-09-13 18:25:00 +0300

    add loading widget, edit auth field and edit success animation

--- lib/core/widgets/loading_widget.dart
+++ lib/core/widgets/loading_widget.dart
@@ -1,34 +1,18 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:lottie/lottie.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../utils/app_assets.dart';
+import '../utils/size_config.dart';
 
-// import '../utils/app_assets.dart';
-// import '../utils/app_text_style.dart';
-// import '../utils/size_config.dart';
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({super.key});
 
-// class LoadingWidget extends StatelessWidget {
-//   const LoadingWidget({
-//     super.key,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           LottieBuilder.asset(
-//             AppAssets.loadingAnimation,
-//             height: AppSize.height(75),
-//           ),
-//           SizedBox(height: AppSize.height(10)),
-//           Text(
-//             'loading'.tr,
-//             textAlign: TextAlign.center,
-//             style: AppTextStyle.green20700,
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: LottieBuilder.asset(
+        AppAssets.loading,
+        height: AppSize.getHeight(75),
+      ),
+    );
+  }
+}
