4499548cafb88a96badef812865de016ed17130c
Ahmed okal <159727160+don-okal@users.noreply.github.com>
2025-12-02 22:51:39 +0200

    feat: Add core application constants and the signup UI component.

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -8,4 +8,5 @@ class AppConstants {
   static const String companyId = '6745f968f31e90c1c3b1a5d8';
   static const String branchId = '6745f9b7f31e90c1c3b1a5e1';
   static const String role = 'active_deer';
+  static const String logoUrl = 'https://api.absat-erp.com/';
 }
--- lib/feature/auth/presentation/views/widgets/signup_body.dart
+++ lib/feature/auth/presentation/views/widgets/signup_body.dart
@@ -1,14 +1,11 @@
 import 'package:active_deer/core/widgets/custom_primary_button.dart';
 import 'package:active_deer/feature/auth/presentation/getx/controllers/auth_controller.dart';
 import 'package:active_deer/feature/auth/presentation/views/widgets/auth_field.dart';
-import 'package:active_deer/feature/auth/presentation/views/widgets/birth_date_field.dart';
-import 'package:active_deer/feature/auth/presentation/views/widgets/terms_andconditions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import '../../../../../core/routes/app_pages.dart';
 import '../../../../../core/theme/app_text_theme.dart';
-import '../../../../../core/utils/app_assets.dart';
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_padding.dart';
 import '../../../../../core/utils/app_validation.dart';
@@ -27,15 +24,6 @@ class SignUpBody extends GetView<AuthController> {
           spacing: AppSize.getHeight(10),
           children: [
             AuthField(
-              hintText: 'enterFullName'.tr,
-              title: 'name'.tr,
-              controller: controller.signUpController.nameController,
-              isObscure: false,
-              validator: (value) => AppValidation.name(value),
-              keyboardType: TextInputType.name,
-              textInputAction: TextInputAction.next,
-            ),
-            AuthField(
               title: 'phoneNumber'.tr,
               hintText: 'enterPhoneNumber'.tr,
               controller: controller.signUpController.phoneController,
@@ -45,95 +33,14 @@ class SignUpBody extends GetView<AuthController> {
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               textInputAction: TextInputAction.next,
             ),
-            AuthField(
-              hintText: 'mail'.tr,
-              title: 'enterMail'.tr,
-              controller: controller.signUpController.emailController,
-              isObscure: false,
-              validator: (value) => AppValidation.email(value),
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-            ),
-            AuthField(
-              hintText: 'enterIdNumber'.tr,
-              title: 'idNumber'.tr,
-              controller: controller.signUpController.idNumberController,
-              isObscure: false,
-              validator: (value) => AppValidation.idNumber(value),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.numberWithOptions(signed: false),
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            ),
-            const BirthDateField(),
-            Obx(
-              () => AuthField(
-                hintText: '******'.tr,
-                title: 'password'.tr,
-                controller: controller.signUpController.passwordController,
-                isObscure:
-                    controller.signUpController.obscurePasswordText.value,
-                validator: (value) => AppValidation.password(value),
-                suffixIcon: Padding(
-                  padding: AppPadding.suffixPadding,
-                  child: InkWell(
-                    onTap: controller.signUpController.togglePasswordVisibility,
-                    child: SvgPicture.asset(
-                      height: AppSize.getHeight(24),
-                      width: AppSize.getWidth(24),
-                      controller.signUpController.obscurePasswordText.value
-                          ? AppAssets.eyeOff
-                          : AppAssets.eyeOn,
-                    ),
-                  ),
-                ),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.visiblePassword,
-              ),
-            ),
-            Obx(
-              () => AuthField(
-                hintText: '******'.tr,
-                title: 'confirmPassword'.tr,
-                controller:
-                    controller.signUpController.confirmPasswordController,
-                isObscure: controller
-                    .signUpController
-                    .obscureConfirmPasswordText
-                    .value,
-                validator: (value) => AppValidation.confirmPassword(
-                  value,
-                  controller.signUpController.passwordController.text,
-                ),
-                suffixIcon: Padding(
-                  padding: AppPadding.suffixPadding,
-                  child: InkWell(
-                    onTap: controller
-                        .signUpController
-                        .toggleConfirmPasswordVisibility,
-                    child: SvgPicture.asset(
-                      height: AppSize.getHeight(24),
-                      width: AppSize.getWidth(24),
-                      controller
-                              .signUpController
-                              .obscureConfirmPasswordText
-                              .value
-                          ? AppAssets.eyeOff
-                          : AppAssets.eyeOn,
-                    ),
-                  ),
-                ),
-                textInputAction: TextInputAction.done,
-                keyboardType: TextInputType.visiblePassword,
-              ),
-            ),
-            const TermsAndConditions(),
-            SizedBox(height: AppSize.getHeight(70)),
+            SizedBox(height: AppSize.getHeight(230)),
             Obx(
               () => CustomPrimaryButton(
                 title: 'signUp'.tr,
-                onTap: controller.signUpController.submitSignUp,
+                onTap: () {
+                  Get.toNamed(Routes.enterPin);
+                },
                 isLoading: controller.signUpController.isLoading.value,
-                isEnable: controller.signUpController.isAgree.value,
               ),
             ),
             SizedBox(height: AppSize.getHeight(10)),
